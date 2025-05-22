8f2dac564dac6c85c0ebcbf15e8f05f6f69265bc
Louis Krumme <louis@krumme-naturgaerten.de>
2025-05-22 17:57:40 +0200

    finished getResourceAsStream and use Stream API in resources()

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /** Starter for the stream api task. */
 public class Main {
@@ -73,8 +74,7 @@ public class Main {
      * @return An open {@link InputStream} for the resource file
      */
     private static InputStream getResourceAsStream(String path) {
-        // TODO
-        throw new UnsupportedOperationException();
+        return Main.class.getResourceAsStream(path);
     }
 
     /**
@@ -88,31 +88,16 @@ public class Main {
      * @return String of all matching lines, separated by {@code "\n"}
      */
     public static String resources(String path) {
-        // TODO
-        StringBuilder result = new StringBuilder();
-
         try (InputStream stream = getResourceAsStream(path)) {
             BufferedReader r = new BufferedReader(new InputStreamReader(stream));
-
-            List<String> allLines = new ArrayList<>();
-
-            String newLine = r.readLine();
-            while (newLine != null) {
-                allLines.add(newLine);
-                newLine = r.readLine();
-            }
-
-            for (int i = 1; i < allLines.size(); i++) {
-                String s = allLines.get(i);
-                if (s.startsWith("a") && !(s.length() < 2)) {
-                    result.append(allLines.get(i)).append("\n");
-                }
-            }
+            return r.lines()
+                .filter(s  -> s.startsWith("a"))
+                .filter(s -> s.length() < 2)
+                .collect(Collectors.joining("\n"));
 
         } catch (IOException e) {
             System.err.println("Ouch, that didn't work: \n" + e.getMessage());
         }
-
-        return result.toString();
+        return null;
     }
 }
